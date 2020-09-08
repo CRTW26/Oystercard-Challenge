d2180a829c4c555607fff5824de2a1f1201b6746
Charlotte Brandhorst-Satzkorn <catzkorn@gmail.com>
2020-09-08 15:13:43 +0100

    creating tests for touching in without enough balance

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -1,30 +1,30 @@
 class Oystercard
-
-  attr_reader :balance, :on_journey
+  attr_reader :balance, :on_journey, :journey_cost
 
   DEFAULT_BALANCE = 0
   MAX_AMOUNT = 90
+  MIN_JOURNEY_COST = 1
 
-    def initialize(balance= DEFAULT_BALANCE)
-      @balance = balance
-      @on_journey = false
-    end
+  def initialize(balance = DEFAULT_BALANCE)
+    @balance = balance
+    @on_journey = false
+  end
 
-    def top_up(amount)
-      raise "Cannot top up: your card reached the limit of £#{MAX_AMOUNT}" if @balance + amount > MAX_AMOUNT
-      @balance += amount
-    end
+  def top_up(amount)
+    raise "Cannot top up: your card reached the limit of GBP#{MAX_AMOUNT}" if @balance + amount > MAX_AMOUNT
+    @balance += amount
+  end
 
-    def spend(cost)
-      @balance -= cost
-    end
+  def spend(cost)
+    @balance -= cost
+  end
 
-    def touch_in
-      @on_journey = true
-    end
+  def touch_in
+    raise "Insufficient balance for journey" if @balance < MIN_JOURNEY_COST
+    @on_journey = true
+  end
 
-    def touch_out
-      @on_journey = false
-    end
-
+  def touch_out
+    @on_journey = false
+  end
 end
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -1,52 +1,56 @@
-require 'Oystercard'
+require "Oystercard"
 
 describe Oystercard do
-
   let(:oystercard) { Oystercard.new(20) }
 
-  describe '#balance' do
-    it 'shows the balance of the card' do
+  describe "#balance" do
+    it "shows the balance of the card" do
       expect(subject.balance).to be 0
     end
   end
 
-  describe '#top_up' do
-    it 'adds money to the card' do
+  describe "#top_up" do
+    it "adds money to the card" do
       expect { subject.top_up(5) }.to change { subject.balance }.by(5)
     end
 
-    it 'raises error if MAX_LIMIT exceeded' do
-      expect { subject.top_up(Oystercard::MAX_AMOUNT + 1) }.to raise_error "Cannot top up: your card reached the limit of £90"
+    it "raises error if MAX_LIMIT exceeded" do
+      expect { subject.top_up(Oystercard::MAX_AMOUNT + 1) }.to raise_error "Cannot top up: your card reached the limit of GBP90"
     end
   end
 
-  describe '#spend' do
-    it 'subtracts cost of journey from balance' do
+  describe "#spend" do
+    it "subtracts cost of journey from balance" do
       expect { subject.spend(2) }.to change { subject.balance }.by(-2)
     end
   end
 
-  describe '#touch_in' do
-    it 'allows user to touch card' do
+  describe "#touch_in" do
+    it "allows user to touch card" do
       expect(oystercard).to respond_to(:touch_in)
     end
 
-    it 'changes on_journey status to true' do
+    it "changes on_journey status to true" do
       oystercard.touch_in
       expect(oystercard.on_journey).to be true
     end
+
+    it "raises an error if card balance is less than minimum amount" do
+      card = Oystercard.new
+      card.touch_in
+      expect { (subject.balance < Oystercard::MIN_JOURNEY_COST) }.to raise_error "Insufficient balance for journey"
+    end
   end
 
-  describe '#touch_out' do
-    it 'allows user to touch out' do
+  describe "#touch_out" do
+    it "allows user to touch out" do
       expect(oystercard).to respond_to(:touch_out)
     end
 
-    it 'changes on_journey to be false' do
+    it "changes on_journey to be false" do
       oystercard.touch_in
       oystercard.touch_out
       expect(oystercard.on_journey).to be false
     end
   end
-
 end
